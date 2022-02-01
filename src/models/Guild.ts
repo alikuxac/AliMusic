bcfbe90f033d669ead0a1bd1013b0c2fc2097ca9
Alikuxac <lexson270400@gmail.com>
2022-02-02 00:52:41 +0700

    fix(model): remove guild prefix

--- src/models/Guild.ts
+++ src/models/Guild.ts
@@ -1,17 +1,13 @@
 import { Document, model, Schema } from 'mongoose';
 
-import { PREFIX } from '#root/configs'
-
 interface IGuild extends Document {
     guildID: string;
-    prefix: string;
     whitelist: boolean;
 
 }
 
 const guildSchema = new Schema({
     guildID: { type: String, required: true, unique: true },
-    prefix: { type: String, default: PREFIX },
     whitelist: { type: Boolean, default: false },
 }, {
     versionKey: false
