f49d58c0a706603d8626636d3fbe02a3b9dc3d49
Alikuxac <lexson270400@gmail.com>
2022-07-06 14:16:46 +0700

    chore(bot): remove whitelist

--- src/listeners/messages/modelChecker.ts
+++ src/listeners/messages/modelChecker.ts
@@ -2,7 +2,6 @@ import { Listener, ListenerOptions, Events } from "@sapphire/framework";
 import { ApplyOptions } from "@sapphire/decorators";
 import type { Message } from "discord.js";
 
-import GuildModels from "#models/Guild";
 import UserModels from "#models/User";
 
 @ApplyOptions<ListenerOptions>({ event: Events.MessageCreate })
@@ -16,16 +15,6 @@ export default class extends Listener {
                 userID: message.author.id,
             });
             await newUser.save();
-        }
-
-        if (message.guild) {
-            const guildModel = await GuildModels.findOne({ guildID: message.guild.id });
-            if (!guildModel) {
-                const newGuild = new GuildModels({
-                    guildID: message.guild.id,
-                });
-                await newGuild.save();
-            }
         }
     }
 }
